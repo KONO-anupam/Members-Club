c175381189c47912daad762d28bd71eff0f681ff
Asher <asherjohn48@gmail.com>
2025-07-12 12:05:25 +0800

    added findByUsername, updateRoleById

--- models/userModel.js
+++ models/userModel.js
@@ -7,25 +7,31 @@ const findAll = async () => {
 
   const { rows } = await db.query('SELECT * FROM users;');
   return rows;
-
 };
 /*
 * @DESC Get user by ID
 */
-const findById = async (id) => {
 
+const findById = async (id) => {
   const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
   return rows[0];
-
 };
+
+/*
+* @DESC Get user by username
+*/
+
+const findByUsername = async (username) => {
+  const { rows } = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+  return rows[0];
+};
+
 /*
 * @DESC Get user by email
 */
 const findByEmail = async (email) => {
-
   const { rows } = await db.query('SELECT * FROM users WHERE email = $1', [email]);
   return rows[0];
-
 };
 /*
 * @DESC Insert a new user
@@ -38,9 +44,8 @@ const create = async (data) => {
     VALUES 
       ($1, $2, $3, $4, $5, $6, $7, $8);
   `;
-  const { id, role_id, firstName, lastName, username, email, password, deactivated } = data;
-  await db.query(query, [id, role_id, firstName, lastName, username, email, password, deactivated]);
-
+  const { id, role_id, firstName, lastName, username, email, hashedPassword, deactivated } = data;
+  await db.query(query, [id, role_id, firstName, lastName, username, email, hashedPassword, deactivated]);
 };
 /*
 * @DESC Update a user by ID
@@ -57,14 +62,30 @@ const updateById = async (id, data) => {
       username = $4,
       email = $5,
       password = $6,
-      deactivated = $7
+      deactivated = $7,
+      updated_at = $8
     WHERE
-      id = $8;
+      id = $9;
   `;
   const { role_id, firstName, lastName, username, email, password, deactivated } = data;
-  await db.query(query, [role_id, firstName, lastName, username, email, password, deactivated, id]);
-
+  await db.query(query, [role_id, firstName, lastName, username, email, password, deactivated, new Date(), id]);
 };
+
+/*
+* @DESC Update a user role by ID
+*/
+const updateRoleById = async (id, role_id) => {
+  const query = `
+    UPDATE
+      users
+    SET
+      role_id = $1
+    WHERE
+      id = $2
+  `;
+  await db.query(query, [role_id, id]);
+}
+
 /*
 * @DESC Delete user by ID
 */
@@ -84,9 +105,11 @@ const deactivateById = async (id) => {
 module.exports = {
   findAll,
   findById,
+  findByUsername,
   findByEmail,
   create,
   updateById,
+  updateRoleById,
   deleteById,
   activateById,
   deactivateById
