eab319fbc218384b874f258d5062b1dc853222fb
Asher <asherjohn48@gmail.com>
2025-07-15 01:22:10 +0800

    check if prod or dev mode

--- database/index.js
+++ database/index.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV == 'production' ? true : false;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: isProduction ? process.env.DATABASE_URL : process.env.DEVELOPMENT_DATABASE_URL,
 });
 
 module.exports = pool;
