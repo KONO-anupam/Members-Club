70f9df9ff283d6c5db79cc508dfef1909c34ec9f
Asher <asherjohn48@gmail.com>
2025-07-14 23:24:27 +0800

    seeds superadmin account

--- database/seeds.sql
+++ database/seeds.sql
@@ -5,3 +5,8 @@ VALUES
   ('admin'),
   ('member'),
   ('guest');
+
+INSERT INTO
+  users (id, role_id, first_name, last_name, username, email, password, deactivated)
+VALUES
+  ('pacTRRHxe4ts1NAOxedtK', 1, 'John Asher', 'Manit', 'ash', 'asherjohn48@gmail.com', '$2b$10$GYXGlgtGMy1zlJalP/N76.oTuqEcyQKhs5Fk2QK1oNLUvyTs1cmr.', false);
